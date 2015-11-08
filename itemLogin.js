ac48447bb2ec1237d47e4d696c6023ce9141a4d5
lifengshuang <b2010000327@gmail.com>
2015-11-08 09:17:00 +0800

    updates

---
+++ itemLogin.js
@@ -0,0 +1,51 @@
+/**
+ * Created by zhanjiyuan on 15/11/8.
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  } = React;
+
+var ItemLogin = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{'Username'}</Text>
+        <TextInput
+          style={styles.input}
+        />
+        <Text style={styles.title}>{'Password'}</Text>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.input}
+        />
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container:{
+    padding:20,
+  },
+  input:{
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  title:{
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontWeight: 'bold',
+  }
+});
+
+module.exports = ItemLogin;
